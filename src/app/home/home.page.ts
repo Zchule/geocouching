d61644356fb2bf337af3a66cde62855470e69c41
Zchule <zulemavp94@gmail.com>
2019-03-04 12:33:23 -0500

    navigation page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(
+    private navCtrl: NavController,
+  ) {
+
+  }
+
+  // navPlace() {
+  //   // this.navCtrl.navigateRoot('place');
+  // }
 }
